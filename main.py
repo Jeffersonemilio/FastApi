05e04788acc973da991ad22aa390ca2385cb8ba4
jefferson de melo <jeffersondemelo@MacBook-Pro-de-jefferson.local>
2025-02-08 17:37:38 -0300

    Atualizando main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to ok FastAPI!"}
+
+@app.get("/add")
+def add(a: float, b: float):
+    return {"operation": "addition", "a": a, "b": b, "result": a + b}
+
+@app.get("/subtract")
+def subtract(a: float, b: float):
+    return {"operation": "subtraction", "a": a, "b": b, "result": a - b}
+
+@app.get("/multiply")
+def multiply(a: float, b: float):
+    return {"operation": "multiplication", "a": a, "b": b, "result": a * b}
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    if b == 0:
+        raise HTTPException(status_code=400, detail="Divisão por zero não é permitida")
+    return {"operation": "division", "a": a, "b": b, "result": a / b}
